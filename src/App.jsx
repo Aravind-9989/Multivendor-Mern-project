acaef67e2cb2ca6e04d073cc4a93e31350cb1c2e
Aravind-9989 <shetty2160@gmail.com>
2024-09-26 16:32:58 +0530

    code

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import LandingPage from './vendor-dashboard/pages/Landingpage'
+import LandingPage from './vendor-dashboard/pages/landingpage'
 import { Routes, Route } from 'react-router-dom';
 
 import "./App.css";
