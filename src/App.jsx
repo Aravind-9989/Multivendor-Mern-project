e0f4c5ec6d62e1885fa37f96cfe120a34b532ca2
Aravind-9989 <shetty2160@gmail.com>
2024-09-26 15:15:07 +0530

    all

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import LandingPage from './vendor-dashboard/pages/Landingpage'
 import { Routes, Route } from 'react-router-dom';
 
 import "./App.css";
-import NavBar from '../src/vendor-dashboard/components/Navbar'
+import NavBar from "../src/vendor-dashboard/components/Navbar"
 import Login from '../src/vendor-dashboard/components/forms/Login'
 import NotFound from './vendor-dashboard/components/Notfound'
 
